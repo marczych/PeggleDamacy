a681b1616785695a9d481d832d80d7be59544562
Marc Zych <marczych@gmail.com>
2014-01-24 22:49:31 -0800

    Add commented out fullscreen
    So it's easy to toggle if we want it.

--- conf.lua
+++ conf.lua
@@ -3,4 +3,6 @@ local Constants = require "utils.constants"
 function love.conf(t)
    t.window.width = Constants.SCREEN_WIDTH
    t.window.height = Constants.SCREEN_HEIGHT
+
+   -- t.window.fullscreen = true
 end
