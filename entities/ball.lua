529b5e8fd84b48663bd6cf3ea0ee5ccc5a636145
Marc Zych <marczych@gmail.com>
2014-01-25 11:39:46 -0800

    Ball/peg collisions

--- entities/ball.lua
+++ entities/ball.lua
@@ -2,25 +2,23 @@ Ball = Class{
    init = function(self)
       self.position = Vector(Constants.SCREEN_WIDTH / 2, 0)
       self.velocity = Vector(200, 0)
+      self.radius = Constants.BALL_INITIAL_RADIUS
    end
 }
 
 function Ball:update(dt)
    self:updatePosition(dt)
 
-   if self.position.y > Constants.SCREEN_HEIGHT - Constants.BALL_INITIAL_RADIUS then
-      self:bounce(Vector(0, -1))
-      self:updatePosition(dt)
+   if self.position.y > Constants.SCREEN_HEIGHT - self.radius then
+      self:bounce(Vector(0, -1), dt)
    end
 
-   if self.position.x < 0 + Constants.BALL_INITIAL_RADIUS then
-      self:bounce(Vector(1, 0))
-      self:updatePosition(dt)
+   if self.position.x < 0 + self.radius then
+      self:bounce(Vector(1, 0), dt)
    end
 
-   if self.position.x > Constants.SCREEN_WIDTH - Constants.BALL_INITIAL_RADIUS then
-      self:bounce(Vector(-1, 0))
-      self:updatePosition(dt)
+   if self.position.x > Constants.SCREEN_WIDTH - self.radius then
+      self:bounce(Vector(-1, 0), dt)
    end
 end
 
@@ -29,15 +27,18 @@ function Ball:updatePosition(dt)
    self.position = self.position + (self.velocity * dt)
 end
 
-function Ball:bounce(normal)
+function Ball:bounce(normal, dt)
    normal = normal:normalized()
 
-   self.velocity = (2 * normal * self.velocity * normal - self.velocity) * -Constants.BALL_BOUNCE_MOMENTUM_RETAIN_PERCENTAGE
+   self.velocity = (2 * normal * self.velocity * normal - self.velocity) *
+    -Constants.BALL_BOUNCE_MOMENTUM_RETAIN_PERCENTAGE
+
+   self:updatePosition(dt)
 end
 
 function Ball:draw()
    love.graphics.setColor(100, 150, 200)
-   love.graphics.circle("fill", self.position.x, self.position.y, Constants.BALL_INITIAL_RADIUS * 2)
+   love.graphics.circle("fill", self.position.x, self.position.y, self.radius * 2)
 end
 
 return Ball
--- states/play.lua
+++ states/play.lua
@@ -17,22 +17,30 @@ local blueBucket = Bucketz(250)
 
 
 function play:enter()
--- Initialize all of the pegs
-for i = 1, 50 do
-   pegs[i] = Peg()
-end
-	
+   -- Initialize all of the pegs
+   for i = 1, 50 do
+      pegs[i] = Peg()
+   end
 end
 
 function play:update(dt)
    ball:update(dt)
    background:update(dt)
    blueBucket:update(dt)  --Test bucket. ok to remove and do something better
+
+   ballAndPegSize = ball.radius + Constants.PEG_RADIUS
+
+   for i, peg in ipairs(pegs) do
+      normal = ball.position - peg.position
+      if normal:len() < ballAndPegSize then
+         ball:bounce(normal, dt)
+         break
+      end
+   end
 end
 
 function play:draw()
    love.graphics.setColor(125, 100, 200)
-   love.graphics.print("Play!", 200, 200)
    
    background:draw()
    ball:draw()
@@ -44,6 +52,9 @@ function play:draw()
 end
 
 function play:keypressed(key, unicode)
+   if key == 'j' then
+      ball.velocity = ball.velocity * 1.1
+   end
 end
 
 return play
