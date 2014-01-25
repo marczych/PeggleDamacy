2fdffc99c0e28630d524d146fea50b7bfa967a5a
KATHYxx <Katherine@.(none)>
2014-01-25 15:32:38 -0800

    things draw

--- entities/peg.lua
+++ entities/peg.lua
@@ -1,9 +1,33 @@
+local possibleSprites = {
+	love.graphics.newImage("assets/images/peg1.png"),
+	love.graphics.newImage("assets/images/peg2.png"),
+	love.graphics.newImage("assets/images/peg3.png"),
+	love.graphics.newImage("assets/images/peg4.png"),
+	love.graphics.newImage("assets/images/peg5.png"),
+	love.graphics.newImage("assets/images/peg6.png"),
+	love.graphics.newImage("assets/images/peg7.png"),
+	love.graphics.newImage("assets/images/peg8.png"),
+	love.graphics.newImage("assets/images/peg9.png"),
+	love.graphics.newImage("assets/images/peg10.png"),
+	love.graphics.newImage("assets/images/peg11.png"),
+	love.graphics.newImage("assets/images/peg12.png"),
+	love.graphics.newImage("assets/images/peg13.png"),
+	love.graphics.newImage("assets/images/peg14.png"),
+	love.graphics.newImage("assets/images/peg15.png"),
+	love.graphics.newImage("assets/images/peg16.png"),
+	love.graphics.newImage("assets/images/peg17.png"),
+	love.graphics.newImage("assets/images/peg18.png"),
+	love.graphics.newImage("assets/images/peg19.png")
+}
+
 Peg = Class{
    init = function(self)
       -- Spawn pegs in a random Vector location
       self.position = Utils.randomPegLocation()
       self.wavelength = Utils.randomWavelength()
       self.color = Utils.wavelengthToRGB(self.wavelength)
+      self.spriteIndex = math.floor( math.random(#possibleSprites))
+      
    end
 }
 
@@ -22,7 +46,9 @@ end
 function Peg:drawAtPosition(position)
    -- TODO: use self.color instead
    love.graphics.setColor(self.color.r, self.color.g, self.color.b)
-   love.graphics.circle("fill", position.x, position.y, Constants.PEG_RADIUS)
+   --love.graphics.circle("fill", position.x, position.y, Constants.PEG_RADIUS)
+   love.graphics.draw(possibleSprites[self.spriteIndex], position.x,
+   	position.y)
 end
 
 -- Used for idiomatic module loading.
